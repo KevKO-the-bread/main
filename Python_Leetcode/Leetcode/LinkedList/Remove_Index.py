cf951070d625d7da637b235e171f3b8f0fd6d279
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2023-12-31 01:17:26 +0100

    Update Remove_Index.py

--- Python_Leetcode/Leetcode/LinkedList/Remove_Index.py
+++ Python_Leetcode/Leetcode/LinkedList/Remove_Index.py
@@ -1,10 +1,46 @@
-
+class Node:
+    # Singly linked node
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 class LinkedList:
-  def remove_last_element(self):
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def reverse(self):
+        curr = self.head
+        prev = None
+        while curr:
+            temp = curr.next  # Save reference to next node
+            curr.next = prev  # Reverse current node's pointer
+            prev = curr  # Update previous node
+            curr = temp  # Update current node
+        temp=self.head 
+        self.head= self.tail 
+        self.tail= temp
+        
+    def remove_last_element(self):
         if self.head is None:
             return
-
         curr = self.head
+        
+        if curr.next is None:
+            self.head=self.head.next
+            curr=None
+            return
+
         while curr.next.next:
             curr = curr.next
 
@@ -15,10 +51,7 @@ class LinkedList:
     def remove_first_element(self):
         if self.head is None:
             return
-        temp=self.head
         self.head=self.head.next
-        temp= None
-        
         
     def remove_any_element(self, value):
         if self.head is None:
@@ -31,3 +64,32 @@ class LinkedList:
             curr=curr.next    
         prev.next=curr.next 
         curr= None
+
+
+    def iterate(self):
+        curr = self.head
+        while curr:
+            val = curr.data
+            yield val
+            curr = curr.next
+
+
+def main():
+    items = LinkedList()
+    items.add(1)
+    items.add(2)
+    items.add(3)
+    
+    items.remove_any_element(2)
+    items.remove_last_element()
+    items.remove_first_element()
+    
+    
+    
+    
+
+    for i in items.iterate():
+        print(i)
+
+if __name__ == "__main__":
+    main()
