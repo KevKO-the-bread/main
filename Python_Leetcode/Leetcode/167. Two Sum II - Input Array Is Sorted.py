345cd94bc5b4f67c08e449c8832af2fe66c280e6
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-01-06 18:41:10 +0100

    Create 167. Two Sum II - Input Array Is Sorted.py

---
+++ Python_Leetcode/Leetcode/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        if not numbers:
+            return
+        
+        l=0
+
+        r=len(numbers)-1
+
+        while l<r:
+            summe=numbers[l]+numbers[r]
+            if summe>target:
+                r-=1
+            elif summe<target:
+                l+=1
+            else: 
+                return l+1, r+1
