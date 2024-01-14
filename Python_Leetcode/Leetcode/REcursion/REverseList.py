17e4d2548312ae7b756d48881c144d1d0bb7b4ab
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-01-14 20:48:13 +0100

    Update REverseList.py

--- Python_Leetcode/Leetcode/REcursion/REverseList.py
+++ Python_Leetcode/Leetcode/REcursion/REverseList.py
@@ -1,4 +1,6 @@
-url=
+url=https://leetcode.com/problems/reverse-linked-list/description/
+
+
 # Recursive implementation of n! (n-factorial) calculation
 def factorial(n):
     # Base case: n = 0 or 1
