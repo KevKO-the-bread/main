a43a624f1dba87dd829354a66d575e5d47e9cf96
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-01-14 20:36:34 +0100

    Update REverseList.py

--- Python_Leetcode/Leetcode/REcursion/REverseList.py
+++ Python_Leetcode/Leetcode/REcursion/REverseList.py
@@ -1,3 +1,4 @@
+url=
 # Recursive implementation of n! (n-factorial) calculation
 def factorial(n):
     # Base case: n = 0 or 1
@@ -6,3 +7,13 @@ def factorial(n):
 
     # Recursive case: n! = n * (n - 1)!
     return n * factorial(n - 1)
+class Solution:
+    def reversedList(self, head)
+
+    if not head:
+        return head
+    newNead=head
+    if head.next:
+        newHead=self.reverseList(head.next)
+        head.next.next=head
+    head.next=None
