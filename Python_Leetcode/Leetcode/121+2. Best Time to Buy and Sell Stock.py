b4e3e350559f1c1a024385231b29e235b7b89bf7
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-01-20 18:55:01 +0100

    Create 121+2. Best Time to Buy and Sell Stock.py

---
+++ Python_Leetcode/Leetcode/121+2. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,16 @@
+url= https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+class Solution(object):
+    def maxProfit(self, prices):
+        l=0
+        r=1
+        maxP=0
+
+        while r<len(prices):
+            if prices[l]<prices[r]:
+                profit=prices[r]-prices[l]
+                maxP=max(maxP, profit)
+            else:
+                l=r
+            r+=1
+        return maxP
