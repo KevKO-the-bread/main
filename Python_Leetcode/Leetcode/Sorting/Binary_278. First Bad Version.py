0dffb197ff4d0116d3955829f4dbbe225ac27ccc
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-01-25 23:13:57 +0100

    Create Binary_278. First Bad Version.py

---
+++ Python_Leetcode/Leetcode/Sorting/Binary_278. First Bad Version.py
@@ -0,0 +1,16 @@
+url=https://leetcode.com/problems/first-bad-version/
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        left, right = 1, n
+
+        while left < right:
+            mid =(left+right) // 2
+           
+           
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
