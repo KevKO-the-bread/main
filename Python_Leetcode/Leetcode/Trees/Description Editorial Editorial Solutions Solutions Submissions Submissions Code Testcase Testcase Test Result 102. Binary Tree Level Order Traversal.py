827e9640e184863df33f5e6fa687c355d4d4d6a4
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-02-04 21:11:07 +0100

    Create Description Editorial Editorial Solutions Solutions Submissions Submissions Code Testcase Testcase Test Result 102. Binary Tree Level Order Traversal.py

---
+++ Python_Leetcode/Leetcode/Trees/Description Editorial Editorial Solutions Solutions Submissions Submissions Code Testcase Testcase Test Result 102. Binary Tree Level Order Traversal.py
@@ -0,0 +1,23 @@
+url= https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+class Solution(object):
+    def levelOrder(self, root):
+        queue=deque()
+    
+        if root:
+            queue.append(root)
+        
+        res=[]
+        while len(queue)>0:
+            level=[]
+        
+            for i in range(len(queue)):
+                curr=queue.popleft()
+                level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            res.append(level)
+
+        return res
