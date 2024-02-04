1571772fdb17421a1d7c71610eb632a9948c1ed2
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-02-04 20:00:17 +0100

    Update 94. Binary Tree Inorder Traversal.py

--- Python_Leetcode/Leetcode/Trees/94. Binary Tree Inorder Traversal.py
+++ Python_Leetcode/Leetcode/Trees/94. Binary Tree Inorder Traversal.py
@@ -12,3 +12,37 @@ class Solution(object):
         self.inorderTraversal(root.right)
 
         return self.root_list
+
+iterative:
+def inorderTraversal(self, root):
+    res = []
+    stack = []
+    cur = root
+    
+    while cur or stack:
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        cur = stack.pop()
+        res.append(cur.val)  # Änderung: Fügen Sie den Wert von cur zur Ergebnisliste hinzu, nicht cur selbst
+        cur = cur.right
+
+    return res  # Änderung: Rückgabe der Ergebnisliste, nicht von cur
+
+
+
+
+def preorder(root):
+    if not root:
+        return    
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root):
+    if not root:
+        return    
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
