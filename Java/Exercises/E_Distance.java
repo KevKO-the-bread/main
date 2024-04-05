ac769a55e3d985319be49e1d7740edfdd80e2bcb
KevKO-the-bread <155004936+KevKO-the-bread@users.noreply.github.com>
2024-04-05 23:46:55 +0200

    Create E_Distance.java

---
+++ Java/Exercises/E_Distance.java
@@ -0,0 +1,28 @@
+public class E_Distance {
+	double x;
+	double y;
+	
+	public E_Distance(double x, double y) {
+		this.x=x;
+		this.y=y;
+	}
+		
+	public double edistance(qwertz other) {
+		double part1 = Math.pow(x - other.x, 2);
+	    double part2 = Math.pow(y - other.y, 2);
+		return Math.sqrt(part1+ part2);
+	}
+	
+    public static void main(String[] args) {
+    	E_Distance p1= new E_Distance(0.2, 15);
+    	E_Distance p2= new E_Distance(13.1, 11);
+    	
+    	double distance;
+    	distance=p1.edistance(p2);
+    	System.out.println(distance);
+        }
+    }
+
+
+
+/**@author Luis Reindlmeier, Kevin Brot, Sarah Rochel**/
